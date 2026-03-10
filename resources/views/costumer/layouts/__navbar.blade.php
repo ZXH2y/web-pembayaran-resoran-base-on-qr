5b9dfc2c8b1423e9779b50dc982622601fef9956
ZXH2y <andri9825559@gmail.com>
2026-03-10 15:53:57 +0800

    integrasi template Cart dan Checkout

--- resources/views/costumer/layouts/__navbar.blade.php
+++ resources/views/costumer/layouts/__navbar.blade.php
@@ -21,14 +21,14 @@
                     </button>
                     <div class="collapse navbar-collapse bg-white" id="navbarCollapse">
                         <div class="navbar-nav mx-auto">
-                            <a href="index.html" class="nav-item nav-link active">Beranda</a>
-                            <a href="shop.html" class="nav-item nav-link">Menu Makanan</a>
+                            <a href="{{ route('home') }}" class="nav-item nav-link active">Beranda</a>
+                            <a href="{{ route('menu') }}" class="nav-item nav-link">Menu Makanan</a>
                             <a href="shop-detail.html" class="nav-item nav-link">Rincian</a>
                             <div class="nav-item dropdown">
                                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Halaman</a>
                                 <div class="dropdown-menu m-0 bg-secondary rounded-0">
-                                    <a href="cart.html" class="dropdown-item">Cart</a>
-                                    <a href="chackout.html" class="dropdown-item">Chackout</a>
+                                    <a href="{{ route('cart') }}" class="dropdown-item">Cart</a>
+                                    <a href="{{ route('checkout') }}" class="dropdown-item">Checkout</a>
                                     <a href="testimonial.html" class="dropdown-item">Testimonial</a>
                                     <a href="404.html" class="dropdown-item">404 Page</a>
                                 </div>
@@ -37,7 +37,7 @@
                         </div>
                         <div class="d-flex m-3 me-0">
                             <button class="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fas fa-search text-primary"></i></button>
-                            <a href="#" class="position-relative me-4 my-auto">
+                            <a href="{{ route('cart') }}" class="position-relative me-4 my-auto">
                                 <i class="fa fa-shopping-bag fa-2x"></i>
                                 <span class="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1" style="top: -5px; left: 15px; height: 20px; min-width: 20px;">3</span>
                             </a>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('costumer.layouts.master');
-});
+})->name('home');
+
+
+Route::get('/menu', function () {
+    return view('costumer.menu');
+})->name('menu');
+
+Route::get('/cart', function () {
+    return view('costumer.cart');
+})->name('cart');
+
+Route::get('/checkout', function () {
+    return view('costumer.checkout');
+})->name('checkout');
