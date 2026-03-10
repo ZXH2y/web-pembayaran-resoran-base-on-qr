d13927d8655030f757cb71772eca8d8136bac2ee
ZXH2y <andri9825559@gmail.com>
2026-03-10 16:08:12 +0800

    database seeder

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Item extends Model
 {
-    use softDeteles, HasFactory;
+    use SoftDeletes, HasFactory;
 
     protected $fillable = [
         'name',
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,12 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+            CategorySeeder::class,
+            ItemSeeder::class,
         ]);
+       
     }
 }
