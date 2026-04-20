41f4fdf603d4066ef408e6af76d69e492137352d
ZXH2y <andri9825559@gmail.com>
2026-04-20 13:27:54 +0800

    Melihat detail pesanan

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Models\OrderItem;
 
 class OrderController extends Controller
 {
@@ -11,5 +12,12 @@ class OrderController extends Controller
         $orders = Order::all()->sortByDesc('created_at');
 
         return view('admin.order.index', compact('orders'));
+    }
+
+    public function show($id){
+        $order = Order::findOrFail($id);
+        $orderItems = OrderItem::where('order_id', $order->id)->get();
+
+        return view('admin.order.show', compact('order', 'orderItems'));
     }
 }
